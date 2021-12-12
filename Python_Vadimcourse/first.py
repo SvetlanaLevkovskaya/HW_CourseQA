f52e221394c607c99d3d4ce097c4e1c818ebb90d
unknown <levkovskayase@gmail.com>
2021-12-12 14:47:53 +0300

    Add first.ry

---
+++ Python_Vadimcourse/first.py
@@ -0,0 +1,43 @@
+# print(1)
+# print(2)
+#
+# print('Svetlana')
+#
+# a = 4
+# if (a > 3):
+#     print('a = ', a)
+#
+# a = 2
+# if (a > 3):
+#     print('a = ', a)
+#
+# # строчные комментарии
+# # блочные комментарии
+# # cntr + / - закомментировать блок
+# # todo add cycle
+#
+# print(1, 'Svetlana', True, '!$(*(*%)_%)')
+#
+# # print(1 + 'Svetlana' + True + '!$(*(*%)_%)') не можем прибавлять число к строке, необходимо привести к одному типу данных
+#
+# print('1' + 'Svetlana' + 'True' + '!$(*(*%)_%)') #конкотинация строки
+#
+# print(str(1) + 'Svetlana' + str(True) + '!$(*(*%)_%)') # явное приведение
+#
+# param = 1                      # заводим переменную
+# print(str(param) + 'Svetlana' + str(True) + '!$(*(*%)_%)')
+
+
+# name = input('Svetlana')
+# print(name)
+
+# записываем скрипт (в Git Bash пишем python first.py, затем вводим имя)
+# name = input()
+# print('My name is = ', name)
+
+# вечный цикл  // cntl + C
+while True:
+    name = input()
+    print('My name is = ', name)
+
+
